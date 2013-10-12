45db6cc198ecf1d536b5131f01df1faec28443b2
Patrik Gothe <patrik.got@gmail.com>
2013-10-12 18:35:34 +0200

    still compiling, auth working..

--- TrickTackyToe/GCHelper.h
+++ TrickTackyToe/GCHelper.h
@@ -9,14 +9,29 @@
 #import <Foundation/Foundation.h>
 #import <GameKit/GameKit.h>
 
-@interface GCHelper : NSObject {
+@protocol GCHelperDelegate
+- (void)matchStarted;
+- (void)matchEnded;
+- (void)match:(GKMatch *)match didReceiveData:(NSData *)data
+   fromPlayer:(NSString *)playerID;
+@end
+
+@interface GCHelper : NSObject <GKMatchmakerViewControllerDelegate, GKMatchDelegate>{
     BOOL gameCenterAvailable;
     BOOL userAuthenticated;
+	UIViewController *presentingViewController;
+	GKMatch *match;
+	BOOL matchStarted;
 }
 
 @property (assign, readonly) BOOL gameCenterAvailable;
+@property (retain) UIViewController *presentingViewController;
+@property (retain) GKMatch *match;
+@property (assign) id <GCHelperDelegate> delegate;
+
 
 + (GCHelper *)sharedInstance;
 - (void)authenticateLocalUser;
+- (void)findMatchWithMinPlayers:(int)minPlayers maxPlayers:(int)maxPlayers viewController:(UIViewController *)viewController delegate:(id<GCHelperDelegate>)theDelegate;
 
 @end
